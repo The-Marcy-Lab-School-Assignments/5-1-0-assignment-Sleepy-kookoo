a3987d063eff019398c54d635573eb588a35ecfe
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-07-09 13:52:10 -0400

    Passed all tests

--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -2,6 +2,36 @@ import getId from "../utils/getId";
 import CartItem from "./CartItem";
 
 class ShoppingCart {
+  #cartItems = [];
+  static #allCarts = [];
+
+  constructor() {
+    this.id = getId();
+
+    ShoppingCart.#allCarts.push(this);
+  }
+
+  createItem(name, price) {
+    const addedItem = new CartItem(name, price);
+    this.#cartItems.push(addedItem);
+    return addedItem;
+  }
+  getItems() {
+    return [...this.#cartItems];
+  }
+  removeItem(id) {
+    const index = this.#cartItems.findIndex((item) => item.id === id)
+    this.#cartItems.splice(index, 1);
+  }
+  getTotal() {
+    return this.#cartItems.reduce((total, { price }) => total + price, 0)
+  }
+  static listAll() {
+    return [...ShoppingCart.#allCarts];
+  }
+  static findBy(id) {
+    return ShoppingCart.#allCarts.find((cart) => cart.id === id);
+  }
 
 }
 
